7584b22aebef52f0e4ef11ee7b588999fd5f9463
TeodoraBuncheva <157591921+TeodoraBuncheva@users.noreply.github.com>
2024-07-03 17:40:54 +0300

    Delete CodeMonkeys directory

--- CodeMonkeys/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeMonkeys.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
