ae81fb4d5dccf9750794f6893746279e91c4a8b0
Stef40IT <stef40tobg@gmail.com>
2024-07-04 11:40:04 +0300

    Reworked the DesignTimeFactory

---
+++ Monkey/Monkey.Data/Data/ApplicationDbContextDesignTimeFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Monkey.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monkey.Data.Data.Entities;
+
+namespace Monkey.Data.Data
+{
+    public class ApplicationDbContextDesignTimeFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            if (args.Length != 1) throw new InvalidOperationException
+                ("You need to pass the connection string to use the only argument!");
+            string connectionString = args[0];
+
+            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            ApplicationDbContext dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            return dbContext;
+        }
+    }
+}
