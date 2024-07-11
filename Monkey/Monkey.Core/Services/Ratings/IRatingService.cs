bda96c7b67c08cf680d32a858786e0e18840d771
Petar Danailov <danailovvpetar@gmail.com>
2024-07-11 14:39:18 +0300

    Added rating

---
+++ Monkey/Monkey.Core/Services/Ratings/IRatingService.cs
@@ -0,0 +1,16 @@
+﻿using Monkey.Data.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkey.Core.Services.Ratings
+{
+    public interface IRatingService
+    {
+        Task AddRatingAsync(Rating rating);
+        Task<Rating> GetRatingByUserAndGameAsync(string userId, int gameId);
+        Task<List<Rating>> GetRatingsByGameAsync(int gameId);
+    }
+}
---
+++ Monkey/Monkey.Core/Services/Ratings/RatingService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Monkey.Data;
+using Monkey.Data.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkey.Core.Services.Ratings
+{
+    public class RatingService:IRatingService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RatingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddRatingAsync(Rating rating)
+        {
+            _context.Ratings.Add(rating);
+            await _context.SaveChangesAsync();
+
+        }
+        public async Task<Rating> GetRatingByUserAndGameAsync(string userId, int gameId)
+        {
+            return await _context.Ratings.FirstOrDefaultAsync(r => r.UserId == userId && r.GameId == gameId);
+        }
+        public async Task<List<Rating>> GetRatingsByGameAsync(int gameId)
+        {
+            return await _context.Ratings.Where(r => r.GameId == gameId).ToListAsync();
+        }
+    }
+}
